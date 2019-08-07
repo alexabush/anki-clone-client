88ccdddf2f7e559f90a27d8917f150600ad538f5
Alexander Bush <alexabush7@gmail.com>
2019-08-07 18:17:54 -0500

    built project

---
+++ build/precache-manifest.88d82e2a0dc0ab0cc961232633c02ba6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b4ca4ff09895ece0b82e",
+    "url": "/static/css/main.160bf9c9.chunk.css"
+  },
+  {
+    "revision": "b4ca4ff09895ece0b82e",
+    "url": "/static/js/main.b4ca4ff0.chunk.js"
+  },
+  {
+    "revision": "02663dd609d4f3f3b864",
+    "url": "/static/js/1.02663dd6.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4ebd0d0c4f819026ea6506f4eb7ea7b5",
+    "url": "/index.html"
+  }
+];
